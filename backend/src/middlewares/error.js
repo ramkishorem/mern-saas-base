2fbabf4d468a5eec6c1f8230fb8e72c15bd3dc62
ramkishorem <ramkishore123@gmail.com>
2019-12-05 18:02:59 +0530

    changed error check to swtich

--- backend/src/middlewares/error.js
+++ backend/src/middlewares/error.js
@@ -3,13 +3,23 @@ import errorLogger from "../errorLogger";
 
 export default function(err, req, res, next) {
   const error = _.pick(err, ["message", "name"]);
-  errorLogger.error(err.message, err);
-  if (error.name === "ValidationError") {
-    res.status(422).json(error);
-  } else if (error.name === "UnauthorizedError") {
-    res.status(403).json(error);
-  } else {
-    res.status(500).json(error);
+  switch (error.name) {
+    case "ValidationError":
+      //mongoose/mongo validation errors
+      res.status(422).json(error);
+      break;
+    case "UnauthorizedError":
+      //eg: token missing on auth header
+      res.status(401).json(error);
+      break;
+
+    default:
+      errorLogger.error(err.message, err);
+      res.status(500).json(error);
+      break;
   }
+  // if (process.env.NODE_ENV !== "production") {
+  //   console.log(err);
+  // }
   next(err);
 }
