9b696a2eb52a8911f33b97d55cf3ae2aa2a81ad0
ramkishorem <ramkishore123@gmail.com>
2019-12-30 17:38:34 +0530

    mock testing for user methods

--- backend/src/users/tests/models.test.js
+++ backend/src/users/tests/models.test.js
@@ -1,4 +1,7 @@
 import User from "../models";
+import jest from "jest-mock";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 describe("selectFields", () => {
   it("should return are firstName, lastName", () => {
@@ -6,19 +9,39 @@ describe("selectFields", () => {
     expect(selectFields).toContain("firstName");
     expect(selectFields).toContain("lastName");
     expect(selectFields.length).toBeLessThan(
-      "firstName".length + "lastName".length + 2
+      "firstName".length + "lastcmderName".length + 2
     );
   });
 });
 
 describe("selectFieldsForUpdate", () => {
-  test("should update and returne firstName, lastName, permissions", () => {
+  it("should update and returne firstName, lastName, permissions", () => {
     const selectFieldsForUpdate = User.selectFieldsForUpdate();
     expect(selectFieldsForUpdate).toEqual({
       firstName: 1,
       lastName: 1,
       permissions: 1
     });
+  });
+});
+
+describe("comparePassword", () => {
+  it("should compare passwords", () => {
+    bcrypt.compare = jest.fn().mockResolvedValue(true);
+    const user = new User({ _id: 1, permissions: ["admin"], password: "a" });
+    user.comparePassword("a");
+    expect(bcrypt.compare).toBeCalled();
+    expect(bcrypt.compare.mock.calls[0][0]).toBe("a");
+  });
+});
+
+describe("generateAuthToken", () => {
+  it("should return a valid Auth token with _id and permissions", () => {
+    const user = new User({ _id: 1, permissions: ["admin"] });
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    expect(decoded).toHaveProperty("_id");
+    expect(decoded).toHaveProperty("permissions");
   });
 });
 
