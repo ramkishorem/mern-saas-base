f65f1896d9e8b3276f149cea1736f97eb40d2c66
ramkishorem <ramkishore123@gmail.com>
2020-01-02 00:24:03 +0530

    made integration test with db actually work

--- backend/src/bin/www.js
+++ backend/src/bin/www.js
@@ -101,3 +101,5 @@ function onListening() {
   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
   debug("Listening on " + bind);
 }
+
+exports.default = server;
--- backend/src/users/tests/routes.test.js
+++ backend/src/users/tests/routes.test.js
@@ -1,25 +1,27 @@
 import request from "supertest";
+import server from "../../bin/www"; // to start db
 import app from "../../app";
+import User from "../models";
 
 describe("/users", () => {
   // beforeEach(() => {
   //   app = require("../../app");
   // });
-  // afterEach(() => app.close());
+  afterEach(async () => {
+    // app.close()
+    await User.remove({});
+  });
 
   describe("GET /", () => {
     // it("should require user to have users:read permission", () => {});
-    it("should return list of users", () => {
-      // const response = await request(app).get("/users");
-      // expect(response.status).toBe(200);
-      // expect(response).toContain();
-      // async await is NOT WORKING
-      request(app)
-        .get("/users")
-        .then(response => {
-          expect(response.statusCode).toBe(200);
-          done();
-        });
+    it("should return list of users", async () => {
+      await User.collection.insertMany([
+        { firstName: "a", lastName: "b", email: "q@w.er" },
+        { firstName: "c", firstName: "d", email: "z@we.r" }
+      ]);
+      const response = await request(app).get("/users");
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(2);
     });
   });
 });
