55f481d0390cca2027c526af8e61a7939fd99427
ramkishorem <ramkishore123@gmail.com>
2020-02-19 20:12:24 +0530

    update permission when inherited/owned permissions change on role

--- backend/src/users/auth/models.js
+++ backend/src/users/auth/models.js
@@ -1,21 +1,83 @@
 import mongoose from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
+import _ from "lodash";
+
+const roleNameSchema = {
+  type: String,
+  required: true,
+  minlength: 3,
+  maxlength: 50,
+  unique: true
+};
 
 const RoleSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50,
-    unique: true
-  },
+  name: roleNameSchema,
   isActive: {
     type: Boolean,
     default: true
+  },
+  ownedPermissions: {
+    type: [String]
+  },
+  inheritedPermissions: [
+    {
+      name: String,
+      parent: roleNameSchema
+    }
+  ],
+  permissions: {
+    type: [String]
   }
 });
 
 RoleSchema.plugin(uniqueValidator);
+
+RoleSchema.pre("save", async function(next) {
+  // Update permissions when ownedPermissions or inheritedPermissions change
+  if (
+    !this.isModified("ownedPermissions") &&
+    !this.isModified("inheritedPermissions")
+  )
+    return next();
+  const ownedPermissions = this.ownedPermissions;
+  const inheritedPermissions = this.inheritedPermissions.map(a => a.name);
+  this.permissions = _.union(ownedPermissions, inheritedPermissions);
+  next();
+});
+
+// RoleSchema.post("save", async function(next) {
+//   // Update permissions when ownedPermissions or inheritedPermissions change
+//   if (!this.isModified("permissions")) return next();
+//   const ownedPermissions = this.ownedPermissions;
+//   const inheritedPermissions = this.inheritedPermissionsArray();
+//   this.permissions = _.union(ownedPermissions, inheritedPermissions);
+//   next();
+// });
+
+RoleSchema.methods.inheritedPermissionsArray = async function() {
+  const perms = this.inheritedPermissions;
+  const inheritedPermissions = perms.map(a => a.name);
+  return inheritedPermissions;
+};
+
+RoleSchema.methods.addPermissions = async function(permissions) {
+  /* Expects array of permissions ({name, parent}) */
+  permissions.forEach(async permission => {
+    if (!permission.parent) {
+      const ownedPermissions = this.ownedPermissions;
+      if (ownedPermissions.includes(permission)) return;
+      this.ownedPermissions.push(permission);
+    } else {
+      const inheritedPermissions = await this.inheritedPermissionsArray();
+      if (inheritedPermissions.includes(permission)) return;
+      this.inheritedPermissions.push({
+        name: permission.name,
+        parent: permission.parent
+      });
+    }
+  });
+  await this.save();
+};
 
 const Role = mongoose.model("Role", RoleSchema);
 
--- backend/src/users/auth/tests/models.test.js
+++ backend/src/users/auth/tests/models.test.js
@@ -30,4 +30,70 @@ describe("RoleSchema", () => {
       }
     });
   });
+
+  describe("inheritedPermissionsArray Method", () => {
+    it("should return an array of inheritedPermissions", async () => {
+      testRole.inheritedPermissions = [
+        { name: "permission1", parent: "parentRole1" }
+      ];
+      const role = new Role(testRole);
+      await role.save();
+      const roles = await role.inheritedPermissionsArray();
+      expect(roles.length).toBe(1);
+      expect(roles[0]).toBe("permission1");
+    });
+  });
+
+  describe("pre Save Method to update permissions", () => {
+    it("should update permissions field when ownedPermissions changes", async () => {
+      const role = new Role(testRole);
+      await role.save();
+      await role.addPermissions(["permission1"]);
+      expect(role.permissions.length).toBe(1);
+      expect(role.permissions).toContain("permission1");
+    });
+    it("should update permissions field when inheritedPermissions changes", async () => {
+      const role = new Role(testRole);
+      await role.save();
+      await role.addPermissions([
+        { name: "permission1", parent: "parentRole" }
+      ]);
+      expect(role.permissions.length).toBe(1);
+      expect(role.permissions).toContain("permission1");
+    });
+  });
+
+  describe("addPermissions Method", () => {
+    it("should add permissions without parent to owned permissions", async () => {
+      const role = new Role(testRole);
+      await role.save();
+      await role.addPermissions(["permission1"]);
+      expect(role.ownedPermissions.length).toBe(1);
+      expect(role.ownedPermissions).toContain("permission1");
+    });
+    it("should add permissions with parent to inherited permissions", async () => {
+      const role = new Role(testRole);
+      await role.save();
+      await role.addPermissions([
+        { name: "permission1", parent: "parentRole" }
+      ]);
+      expect(role.inheritedPermissions.length).toBe(1);
+      expect(role.inheritedPermissions[0].name).toBe("permission1");
+      expect(role.inheritedPermissions[0].parent).toBe("parentRole");
+    });
+    it("should be add permissions of both kind at the same time", async () => {
+      const role = new Role(testRole);
+      await role.save();
+      await role.addPermissions([
+        { name: "permission0" },
+        { name: "permission2" },
+        { name: "permission1", parent: "parentRole" }
+      ]);
+      expect(role.ownedPermissions.length).toBe(2);
+      expect(role.inheritedPermissions.length).toBe(1);
+      expect(role.ownedPermissions).toContain("permission0");
+      expect(role.inheritedPermissions[1].name).toBe("permission1");
+      expect(role.inheritedPermissions[1].parent).toBe("parentRole");
+    });
+  });
 });
